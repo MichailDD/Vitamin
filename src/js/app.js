5f6fdbe9f36be0cdd8c23cc93df3221e49e80e31
Mike Derevyanko <mike21mikee@gmail.com>
2022-11-01 17:23:36 +0200

    new section

--- src/js/app.js
+++ src/js/app.js
@@ -29,8 +29,7 @@ flsFunctions.isWebp();
 // flsFunctions.addTouchClass();
 /* Добавление loaded для HTML после полной загрузки страницы */
 // flsFunctions.addLoadedClass();
-/* Модуль для работы с меню (Бургер) */
-// flsFunctions.menuInit();
+/* Модуль для работы с меню (Бургер) */ flsFunctions.menuInit();
 /* Учет плавающей панели на мобильных устройствах при 100vh */
 // flsFunctions.fullVHfix();
 
@@ -204,7 +203,7 @@ import * as flsScroll from "./files/scroll/scroll.js";
 
 /* Динамический адаптив */
 // Документация: https://template.fls.guru/template-docs/dinamicheskij-adaptiv.html
-// import "./libs/dynamic_adapt.js";
+import "./libs/dynamic_adapt.js";
 
 /* Форматирование чисел */
 // import './libs/wNumb.min.js';
